a446b5da5a85f584df69d6991ee3684bb2b190cf
hdnng <hidng232@gmail.com>
2025-03-09 15:05:52 +0700

    update login

--- QuanLyKhachSan/Program.cs
+++ QuanLyKhachSan/Program.cs
@@ -16,6 +16,13 @@ builder.Services.AddScoped<GenericFunction<User>>(); // GenericFunction cho User
 builder.Services.AddScoped<GenericFunction<Customer>>(); // GenericFunction cho Customer
 builder.Services.AddScoped<GenericFunction<Employee>>(); // GenericFunction cho Employee
 
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30); // Thời gian hết hạn session
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
 // Đăng ký UserManagementService và CustomerManagementService vào DI container
 builder.Services.AddScoped<UserManagementService>(); // Xử lý nghiệp vụ đăng ký User
 builder.Services.AddScoped<CustomerManagementService>(); // Xử lý nghiệp vụ đăng ký Customer
@@ -37,10 +44,11 @@ app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthorization();
 
+app.UseSession();
 // Định nghĩa route mặc định cho AccountController
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Account}/{action=SignUp}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 // Chạy ứng dụng
 app.Run();
